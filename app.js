3f88b12c18ea4b6fb4d56067b70d5393486b3045
AMANverma9118 <amanverma9118359330@gmail.com>
2023-11-05 03:02:24 +0530

    Taking information from post and storing in mongodb

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const http = require('http');
-const fs = require('fs');
-
+const http = require("http");
+const Express = require("express");
+const app = Express();
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-type','text/html');
+const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
+const kittySchema = new mongoose.Schema({
+    Name: String,
+    Email: String
+  });
+
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+main().then(res=>console.log("db connected successfuly"));
+main().catch(err => console.log("db not connected..!!!",err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Login-and-Signup');
+
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+app.use(bodyParser.json()); // Parses JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded form data
+
+app.get('/',(req,res)=>{
+    res.send('Hello everyone');
 })
 
-server.listen(port,hostname,()=>{
-    console.log("The server is hoted successfully");
-});
+app.post('/signup',(req,res)=>{
+    console.log(req.body)
+    const {Name,Email} = req.body
+    Kitten.create({Name,Email})
+    res.send('Singup successfully');
+})
+
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}`); 
+})
